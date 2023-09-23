cadd44d28bf2229037602e34327deeebd523734b
Kaan Alptakin <92170029+kaanalptkn@users.noreply.github.com>
2023-09-23 19:21:30 +0100

    Added 3rd task

--- python.py
+++ python.py
@@ -12,3 +12,5 @@ if n % 2 == 1:   # Check if n is add
     print('Weird')
 elif n % 2 == 0 and 2 >= n >= 5: # Check n is even and inclusive range(>=) [2,5]
     print('Not Weird')
+elif n % 2 == 0 and 6 >= n >= 20: #Check n is even and inclusive range(>=) [6,20]
+    print('Weird')
