d22993304c4fd02bad70b575ca08d513fae80a95
Kaan Alptakin <92170029+kaanalptkn@users.noreply.github.com>
2023-09-23 19:22:59 +0100

    Solved last task

--- python.py
+++ python.py
@@ -14,3 +14,5 @@ elif n % 2 == 0 and 2 >= n >= 5: # Check n is even and inclusive range(>=) [2,5]
     print('Not Weird')
 elif n % 2 == 0 and 6 >= n >= 20: #Check n is even and inclusive range(>=) [6,20]
     print('Weird')
+elif n % 2 == 0 and n > 20: # Check n is even and greater than 20
+    print('Not Weird')
